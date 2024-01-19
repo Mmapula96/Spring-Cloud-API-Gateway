1c6d7ea3b0f11bb54618b5583a1bdeb8556fb7cc
Mmapula96 <111009486+Mmapula96@users.noreply.github.com>
2024-01-19 11:30:34 +0200

    api gateway repo

---
+++ src/main/java/api/gateway/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package api.gateway.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@CrossOrigin
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
+
